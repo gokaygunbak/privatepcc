722d36a8fe945dd5b8829cf5df80170dea7c68e4
Gökay Günbak <gokaygunbak@gmail.com>
2026-01-06 11:41:49 +0300

    database schemas guncellendi

--- sources_202512302148.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO public.sources ("name",url,topic_id,last_fetched_at) VALUES
-	 ('ShiftDelete','https://shiftdelete.net/feed',1,NULL),
-	 ('BBC Technology','https://feeds.bbci.co.uk/news/technology/rss.xml',1,NULL),
-	 ('Sabah Spor','https://www.sabah.com.tr/rss/spor.xml',3,'2025-12-29 16:00:41.903974'),
-	 ('Haberturk Spor','https://www.haberturk.com/rss/spor.xml',3,'2025-12-29 17:06:13.911211'),
-	 ('BBC Türkçe','https://feeds.bbci.co.uk/turkce/rss.xml',4,'2025-12-29 17:06:13.911211');
